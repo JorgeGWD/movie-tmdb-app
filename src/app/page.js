3564aec1ca049c95a05b7858b1920ad2ba6e581a
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-08-28 16:50:21 -0500

    Install axios, consume TMDB API

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,27 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+
+import React, { useEffect, useState } from "react"
+import axios from 'axios'
 
 export default function Home() {
+
+  const [movies, setMovies] = useState([])
+
+  const API_KEY = '70b4fae10e1f2ae4e4fb74fc69dcbd73'
+  const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+  const IMAGE_URL = 'https://image.tmdb.org/t/p/original/'
+
+  useEffect(() => {
+    axios.get(API_URL)
+    .then((response) => {
+      setMovies(response.data.results)
+      console.log(response.data.results)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }, [])
+
   return (
     <>
     <h1>Velope OTT Developer Remote Test</h1>
