56e9c6af27dbe07926cde710c40b7050660bf742
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-08-29 17:08:00 -0500

    Move through the cards with arrow keys, selected css

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react"
 import axios from 'axios'
 import './card.css'
 import { API_URL, IMAGE_URL } from "@/services/api_info"
+import Image from "next/image"
 
-function Card(props) {
+function Card() {
 
     const [movies, setMovies] = useState([])
   
@@ -16,16 +17,38 @@ function Card(props) {
       .catch((err) => {
         console.log(err)
       })
-    }, [API_URL])
+    }, [])
 
+    const [selectedCardIndex, setSelectedCardIndex] = useState(0);
+
+    useEffect(() => {
+      const handleKeyPress = (event) => {
+        const key = event.key.toLowerCase();
+        if (key === 'arrowright') {
+          setSelectedCardIndex((prevIndex) => (prevIndex + 1) % 20);
+        } else if (key === 'arrowleft') {
+          setSelectedCardIndex((prevIndex) =>
+            (prevIndex - 1 + 20) % 20
+          );
+        }
+      };
+  
+      window.addEventListener('keydown', handleKeyPress);
+      return () => {
+        window.removeEventListener('keydown', handleKeyPress);
+      };
+    }, []);
+    
   return (
     <>
-      {movies && movies.length > 0 && movies.map((movie, key) => (
-        <div className="movie-card" key={movie.id}>
-          <img src={`${IMAGE_URL}${movie.poster_path}`}/>
-          <small>{movie.release_date}</small>
-          <h3>{movie.title}</h3>
-          <small>{movie.vote_average}</small>
+      {movies && movies.length > 0 && movies.map((movie, index) => (
+        <div className={`movie-card ${selectedCardIndex === index ? 'selected' : ''}`} key={index} >
+          <img src={`${IMAGE_URL}${movie.poster_path}`} alt={movie.title}/>
+          <div className="movie-card__description">
+            <small>{movie.release_date}</small>
+            <h3>{movie.title}</h3>
+            <small>{movie.vote_average}</small>
+          </div>
         </div>
       ))}
     </>
