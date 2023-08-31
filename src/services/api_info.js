f7295a63585c9bf395041565d60fb4aa1022f4ce
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-08-31 18:19:12 -0500

    Move fetch logic to api_info file

--- src/services/api_info.js
+++ src/services/api_info.js
@@ -1,3 +1,17 @@
-export const API_KEY = '70b4fae10e1f2ae4e4fb74fc69dcbd73'
-export const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+import axios from 'axios'
+
+const API_KEY = '70b4fae10e1f2ae4e4fb74fc69dcbd73'
+const API_URL = 'https://api.themoviedb.org/3';
 export const IMAGE_URL = 'https://image.tmdb.org/t/p/original/'
+
+export const getCardDetails = async (requestData) => {
+    try {
+        const response = await axios.get(
+            `${API_URL}/${requestData}?api_key=${API_KEY}`
+        );
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching movie details:', error);
+        return null;
+    }
+};
