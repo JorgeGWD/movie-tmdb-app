5e65e73007e2967ec5b46f02d67fae491c11eb0a
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-11-13 16:50:59 -0500

    Adjust image url

--- src/services/api_info.js
+++ src/services/api_info.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const API_KEY = '70b4fae10e1f2ae4e4fb74fc69dcbd73'
 const API_URL = 'https://api.themoviedb.org/3';
-export const IMAGE_URL = 'https://image.tmdb.org/t/p/original/'
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/original'
 
 export const getCardDetails = async (requestData) => {
     try {
